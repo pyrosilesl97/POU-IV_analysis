c4c15f0b9e75f5da9ba9f3f4afab1186bb4908f0
Pablo Yamild Rosiles Loeza <pyrosilesl97@gmail.com>
2021-09-28 17:22:08 -0500

    Add the fisrt header and file

---
+++ R/Data_analysis.R
@@ -0,0 +1,16 @@
+
+# Title:
+  #"Cnidarian hair cell development illuminates an ancient role for the class IV POU transcription factor in defining mechanoreceptor identity"
+# Code author:
+  # Pablo Yamild Rosiles Loeza
+  # For information please go to https://pyrosilesl97.github.io/
+# Date: "9/28/2021"
+
+
+
+
+
+options(width = 120)
+pkgs <- loadedNamespaces()
+pkgs <- installed.packages()[, "Package"]
+sessioninfo::session_info(pkgs)
